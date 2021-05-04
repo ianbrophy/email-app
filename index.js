bd3ce267e09f5e1759de190d4401f44e5430917d
ian <ianbrophy@gmail.com>
2021-05-04 15:19:13 -0600

    update to index

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express")
 const app = express()
 app.get("/", (req, res) => {
-  res.send({ hi: "there" })
+  res.send({ hi: "there!" })
 })
 // app.listen(5000)
 app.listen(process.env.PORT || 5000)
